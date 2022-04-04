a78dd9e96b472a1c9013885b92b186e7c3e06b00
Braden Borman <bborman@shelterinsurance.com>
2022-04-04 17:47:40 -0500

    Cards flip but i want to change

--- bluechips-client/src/components/app/app.tsx
+++ bluechips-client/src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { WagerBuilder } from "../wagerbuilder/wagerBuilder";
 import { WagerReview } from "../wagerreview/wagerReview";
+import { flatMap } from "lodash";
 
 require("./app.scss");
 
@@ -10,7 +11,24 @@ export const App: React.FC<AppProps> = (props: AppProps) => {
   return (
     <>
       {/* <WagerBuilder /> */}
-      <WagerReview cost={100} payout={1000} from={"Braden Borman"} />
+      <WagerReview
+        cost={100}
+        payout={1000}
+        from={"Braden Borman"}
+        bet={
+          "Jamar Chase will have 2 tds and 100 rec-yards AND Tom Brady will throw for 5 tds"
+        }
+        accepted={false}
+        actionDate="12/4/22"
+      />
+      <WagerReview
+        cost={100}
+        payout={1000}
+        from={"Braden Borman"}
+        bet={"Jamar Chase will have 2 tds and 100 rec-yards"}
+        accepted={false}
+        actionDate="12/4/22"
+      />
     </>
   );
 };
--- bluechips-client/src/components/wagerreview/wagerReview.tsx
+++ bluechips-client/src/components/wagerreview/wagerReview.tsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import classNames from "classnames";
 
 export interface WagerReviewProps {
   cost: number;
   payout: number;
+  bet: string;
   from: string;
+  accepted: boolean;
+  actionDate: string;
 }
 
 export const WagerReview: React.FC<WagerReviewProps> = (
   props: WagerReviewProps
 ) => {
-  return (
-    <div className="container">
+  const [flipped, setFlipped] = useState<boolean>(props.accepted);
+  const [accepted, setAccepted] = useState<boolean>(props.accepted);
+
+  const handleClick = (userAcceptedWager: boolean) => {
+    setAccepted(userAcceptedWager);
+    setFlipped(true);
+  };
+
+  const frontCard = (
+    <div className="front">
       <h2>You've recived a wager offer!</h2>
       <div>
         {props.from} has offered you the following to wager:
-        <div className="bet">Jamar Chase will have 2 tds and 100 rec-yards</div>
+        <div className="bet">{props.bet}</div>
         <div className="money-details">
           <span className="spotlight cost">Cost:</span> $
           {props.cost.toLocaleString()}
@@ -26,6 +38,26 @@ export const WagerReview: React.FC<WagerReviewProps> = (
           *If you accept this wager, ${props.cost.toLocaleString()} will be
           withdrawn from you reserves.
         </div>
+      </div>
+      <br />
+      <button onClick={e => handleClick(true)}>Accept</button>
+      <button onClick={e => handleClick(false)}>Decline</button>
+    </div>
+  );
+
+  const backCard = (
+    <div className="back">
+      You have {accepted ? "acepted" : "declined"} this wager:
+      <div className="wager">{props.bet}</div>
+      <div className="effective-date">{props.actionDate}</div>
+    </div>
+  );
+
+  return (
+    <div className="container">
+      <div className={classNames("card", { flipped: flipped })}>
+        {frontCard}
+        {backCard}
       </div>
     </div>
   );
